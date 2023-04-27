83bebf57008c0db46c27c29e77bc0e8cd9d794a1
Sanket <sanketwarbhe2001@gmail.com>
2023-04-27 11:10:46 +0530

    initial commit

---
+++ CrudOperationIn.NetCore/Models/BrandContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperationIn.NetCore.Models
+{
+    public class BrandContext : DbContext
+    {
+        public BrandContext(DbContextOptions<BrandContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Brand> Brands {get;set;}
+    }
+}
